58a5f96f3787d0f413e4b7e338f94a430ff27735
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-17 22:23:31 -0500

    Struggling to get concepts for Javascript Event listeners

--- JavaScript/Accordion/app.js
+++ JavaScript/Accordion/app.js
@@ -1,1 +1,18 @@
-console.log(document.querySelector('.hidden'));
+const arrow = document.querySelector('.arrow-icon').addEventListener('click', onClick);
+
+// const answer = document.querySelectorAll('p.hidden').addEventListener('click,', onClick());
+
+function onClick(e){
+  // 
+  
+  let val;
+
+  val = e;
+
+  val = e.target.classList;
+
+//   val.className.toggle('show');
+
+  console.log(val);
+}
+
