5c5b8a797ecb6fd3e842176a094d2333ea71f39f
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-18 01:24:11 -0500

    removed console.log message from inside function

--- JavaScript/Accordion/app.js
+++ JavaScript/Accordion/app.js
@@ -1,5 +1,5 @@
- const answers = document.querySelectorAll('p.answer.hidden');
- answersArr = Array.from(answers);
+//  const answers = document.querySelectorAll('p.answer.hidden');
+//  answersArr = Array.from(answers);
 
  const buttons = document.getElementsByTagName('button');
  
@@ -7,7 +7,6 @@ document.body.addEventListener('click', showAnswer);
 
 function showAnswer(e){
   if(e.target.parentElement.classList.contains('btn')){
-    console.log(buttons);
     e.target.parentElement.parentElement.nextElementSibling.classList.toggle('show');
   }
 
