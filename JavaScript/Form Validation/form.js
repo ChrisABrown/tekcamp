b381fb3f9ac486bd67b558e43b444cf0b842839f
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-18 22:38:14 -0500

    Last commit of the night, couldn't figure out how to prevent form from reloading or change visibility of error messages

--- JavaScript/Form Validation/form.js
+++ JavaScript/Form Validation/form.js
@@ -1,16 +1,18 @@
 const container = document.querySelector('#form');
 const inputs = container.querySelectorAll('form > input');
 const errors = container.querySelectorAll('form > small');
+const submit = container.querySelector('#btn');
 
 
 inputsArr = Array.from(inputs);
 
-container.addEventListener('submit', checkInputs);
+submit.addEventListener('submit', checkInputs);
 
 function checkInputs(e) {
   for(let i = 0; i < inputsArr.length; i++){
   if(inputsArr[i].value === ' '){
-   console.log(inputsArr[i]);
+    e.preventDefault();
+    console.log(inputsArr);
 
 
   }
