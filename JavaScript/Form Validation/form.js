dabeaa969f6a315b3bcf6a430b4ebedf212b99c9
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-18 20:11:08 -0500

    Made const variables for name, email, password in the form.js file

--- JavaScript/Form Validation/form.js
+++ JavaScript/Form Validation/form.js
@@ -1,21 +1,5 @@
-document.querySelector('#btn').addEventListener('click', showErrors(e))
-{
-  console.log('Hello World');
-
-  e.preventDefault();
-
-};
-
-// function showErrors(e){
-//   if(e.target.parentElement.classList.contains(''))
-
-
-
-//     document.body.addEventListener('submit', showErrors);
-
-// function showErrors(e){
-//   if(e.target.parentElement.classList.contains('btn')){
-//     e.target.parentElement.parentElement.nextElementSibling.classList.toggle('show');
-//   }
-
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
