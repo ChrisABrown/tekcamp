2ee87cfd15dba25a12be802dbcb426c557f6cafb
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-19 15:30:34 -0500

    fleshed out code for form validation hw, just need to adjust code for error messages

--- JavaScript/Form Validation/form.js
+++ JavaScript/Form Validation/form.js
@@ -1,19 +1,33 @@
-const container = document.querySelector('#form');
-const inputs = container.querySelectorAll('form > input');
-const errors = container.querySelectorAll('form > small');
-const submit = container.querySelector('#btn');
+const form = document.querySelector('#form form');
+const inputs = document.querySelectorAll('input');
+const errors = document.querySelectorAll('.error')
 
 
-inputsArr = Array.from(inputs);
-
-submit.addEventListener('submit', checkInputs);
-
-function checkInputs(e) {
-  for(let i = 0; i < inputsArr.length; i++){
-  if(inputsArr[i].value === ' '){
-    e.preventDefault();
-    console.log(inputsArr);
 
 
-  }
-}};
+form.addEventListener('submit', (e) => {
+  showError();
+  showEmailError();
+  e.preventDefault();
+
+});
+
+function showError(){
+  for(let i = 0; i < inputs.length; i++){
+    if (inputs[i].value === ''){
+     errors[i].style.visibility = 'visible';
+    }
+    else{ errors[i].style.visibility = 'hidden';
+    }}
+  };
+
+function showEmailError() {
+  let email = document.getElementById('email');
+  let emailError = document.getElementById('email-error');
+  let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(regEx.test(email.value) === false){
+      emailError.textContent = ('Looks like this is not a valid email');
+    }
+    
+
+}
