6a38b73ba58466ffcb052662943892eca764af0c
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-20 17:25:13 -0500

    Final commit, adjusted function for showing errors again to trim whitespace, adjusted email validation function with help from abram by setting error message to a method and using that in the function

--- JavaScript/Form Validation/form.js
+++ JavaScript/Form Validation/form.js
@@ -1,39 +1,44 @@
 const form = document.querySelector('#form form');
-const inputs = document.querySelectorAll('input');
-const errors = document.querySelectorAll('.error');
-const origEmailError = document.querySelector('#email-error');
+const inputs = form.querySelectorAll('input');
+const errors = form.querySelectorAll('.error');
+const origEmailError = form.querySelector('#email-error').textContent;
 
+// Shout out to Abram for this idea for saving the OG error message!
+function resetErrors(){
+  origEmailError;
+};
 
 
 form.addEventListener('submit', (e) => {
+  resetErrors();
   showError();
   validEmailError();
   e.preventDefault();
-
+  
 });
-
+// Shout out to Alex and David for help working through this function, trim wasn't working at first until they showed my how to write it properly
 function showError(){
   for(let i = 0; i < inputs.length; i++){ 
     let text = inputs[i].value.trim();
   if(text === '') {
     errors[i].style.visibility = 'visible';
-    // e.stopPropagation();
+    
   }else{
     errors[i].style.visibility = 'hidden';
-  } }
-}
+  }}};
 
+// BIG Shout out to Abram for this function, couldn't quite figure out how to switch back an forth but his insight unlocked the ideation here
 function validEmailError() {
   const emailInput = document.querySelector('#email');
-  console.log(emailInput);
   const emailError = document.getElementById('email-error');
   const regEx = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  if (emailError.style.visibility === "visible"){
+    emailError.textContent = origEmailError;
+    return
+  };
   if(regEx.test(email.value) === false){
     emailError.innerHTML = 'Looks like this is not a valid email';
     emailError.style.visibility = "visible";
-  }else {
-  
-    
-    
-  }}
+    console.log(emailInput.value);
+  }};
   
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tekcamp
+Resources
+Abram, Nancy and Abigail during Office Hours
+Alex and David, during #help channel huddles, along with everyone else during those huddles
+w3schools.com
+stackoverflow.com
+Udemy videos and course for base knowledge and understanding
+Youtube.com
+all the links and insight from the Slack #help channel
