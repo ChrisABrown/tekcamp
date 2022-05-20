4aad54f30cd224c3babb2dd4bfd5ec8a878ac223
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-05-20 15:02:13 -0500

    adjusted function showError, original function used a different methood with variables to target empty strings, new function does the same but trims the whitespace

--- JavaScript/Form Validation/form.js
+++ JavaScript/Form Validation/form.js
@@ -1,8 +1,7 @@
 const form = document.querySelector('#form form');
 const inputs = document.querySelectorAll('input');
 const errors = document.querySelectorAll('.error');
-const origEmailError = document.querySelector('#email-error').textContent;
-
+const origEmailError = document.querySelector('#email-error');
 
 
 
@@ -14,26 +13,27 @@ form.addEventListener('submit', (e) => {
 });
 
 function showError(){
-  for(let i = 0; i < inputs.length; i++){
-    if (inputs[i].value === ''){
-     errors[i].style.visibility = 'visible';
-    }
-    else{ errors[i].style.visibility = 'hidden';
-    }}
-  };
-
-
+  for(let i = 0; i < inputs.length; i++){ 
+    let text = inputs[i].value.trim();
+  if(text === '') {
+    errors[i].style.visibility = 'visible';
+    // e.stopPropagation();
+  }else{
+    errors[i].style.visibility = 'hidden';
+  } }
+}
 
 function validEmailError() {
-  let email = document.getElementById('email');
-  let emailError = document.getElementById('email-error');
-  let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(regEx.test(email.value) === false){
-      emailError.innerHTML = "Looks like this is not a valid email";
-    }else {
-      return;
+  const emailInput = document.querySelector('#email');
+  console.log(emailInput);
+  const emailError = document.getElementById('email-error');
+  const regEx = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  if(regEx.test(email.value) === false){
+    emailError.innerHTML = 'Looks like this is not a valid email';
+    emailError.style.visibility = "visible";
+  }else {
+  
     
-      }
-  }
-// emailError.visibility = "visible";
-// console.log(emailInput);
+    
+  }}
+  
