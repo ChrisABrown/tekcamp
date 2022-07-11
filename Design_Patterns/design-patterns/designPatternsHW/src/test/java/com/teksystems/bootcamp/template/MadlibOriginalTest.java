b21ec62836301f3d387d3e46263d55df3af598e8
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-07-11 02:05:45 -0500

    Commit 26, madlib may not require tests as coverage is very high, plan to tackle facade and facade tests tomorrow

---
+++ Design_Patterns/design-patterns/designPatternsHW/src/test/java/com/teksystems/bootcamp/template/MadlibOriginalTest.java
@@ -0,0 +1,20 @@
+package com.teksystems.bootcamp.template;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MadlibOriginalTest {
+
+  @Test
+  public void getAdjective() {
+  }
+
+  @Test
+  public void getNoun() {
+  }
+
+  @Test
+  public void getNumber() {
+  }
+}
---
+++ Design_Patterns/design-patterns/designPatternsHW/src/test/java/com/teksystems/bootcamp/template/MadlibSwitchTest.java
@@ -0,0 +1,20 @@
+package com.teksystems.bootcamp.template;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MadlibSwitchTest {
+
+  @Test
+  public void getAdjective() {
+  }
+
+  @Test
+  public void getPluralMagicalCreature() {
+  }
+
+  @Test
+  public void getNumber() {
+  }
+}
