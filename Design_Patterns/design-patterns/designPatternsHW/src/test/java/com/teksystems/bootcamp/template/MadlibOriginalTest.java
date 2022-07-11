2f51ec913f8b321d22bb943a33a8ed5ec1b359ad
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-07-11 12:11:06 -0500

    Commit 27, starting on tests for madlib

--- Design_Patterns/design-patterns/designPatternsHW/src/test/java/com/teksystems/bootcamp/template/MadlibOriginalTest.java
+++ Design_Patterns/design-patterns/designPatternsHW/src/test/java/com/teksystems/bootcamp/template/MadlibOriginalTest.java
@@ -7,7 +7,14 @@ import static org.junit.Assert.*;
 public class MadlibOriginalTest {
 
   @Test
-  public void getAdjective() {
+  public void getAdjective(String[] x, int index) {
+    String str = "";
+    String expResult = "";
+    for (int i = 0; i < x.length; i++) {
+      if (i == index){
+        x[i] == expResult;
+      }
+    }
   }
 
   @Test
