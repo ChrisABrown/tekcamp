fc265d8333e953c08a3c49dafc7559c9fc8342f5
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-07-13 00:21:31 -0500

    Commit 12

--- SQL_exercises/1_num_stars/Exercise1:Day1.sql
+++ SQL_exercises/1_num_stars/Exercise1:Day1.sql
@@ -10,11 +10,14 @@ CREATE TABLE
 INSERT INTO 
 	ratings(num_of_stars, rating_comment) 
 VALUES	  
-	(0, 'Worst movie I have ever seen'),
-	(1, 'Horrible movie'),
-	(2, 'Dont waste your time'),
-	(3, 'Not bad, not that good either'),
-	(4, 'Good movie'),
-	(5, 'Best movie I have ever seen');
-SELECT * 
+	(0, "Worst movie I've ever seen"),
+	(1, "Horrible movie"),
+	(2, "Dont waste your time"),
+	(3, "Not bad, not that good either"),
+	(4, "Good movie"),
+	(5, "Best movie I've ever seen");
+SELECT 
+	rating_id AS 'rating ID', 
+	num_of_stars AS stars,
+	rating_comment AS rating
 FROM ratings;
