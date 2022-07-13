10b75e0e77f1817ca2bf240e791d73c732b6101a
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-07-13 12:37:31 -0500

    Commit 17, finished with exercise 5 and added files to folders

--- SQL_exercises/5_customer_search/Exercise5:Day3.sql
+++ SQL_exercises/5_customer_search/Exercise5:Day3.sql
@@ -1,2 +1,21 @@
 USE sakila;
-SELECT customer_id, first_name, last_name FROM customer WHERE first_name LIKE  '%a%' ;
+SELECT 
+  customer_id, 
+  first_name, 
+  last_name 
+FROM 
+  customer 
+WHERE 
+  (
+    (
+      first_name LIKE '%c%' 
+      OR '%cb%'
+    ) 
+    AND (
+      last_name LIKE '%c%' 
+      OR '%cb%'
+    )
+  );
+
+
+
