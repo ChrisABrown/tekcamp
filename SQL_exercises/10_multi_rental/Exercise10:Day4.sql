af8cd536c61cec5ce92eabbdb328efcc0b3bd99c
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2022-07-15 11:30:58 -0500

    Commit 25, finished exercise 10, with help from Kathleen

--- SQL_exercises/10_multi_rental/Exercise10:Day4.sql
+++ SQL_exercises/10_multi_rental/Exercise10:Day4.sql
@@ -1,16 +1,17 @@
 USE sakila;
 SELECT 
-    customer.customer_id,
     CONCAT(customer.first_name,
             ' ',
-            customer.last_name) AS customer_name,
-rental.rental_id,
-    DATE(rental.rental_date) AS 'rental date',
-    SUM(amount)
+            customer.last_name) AS 'customer name',
+    rental.customer_id,
+    COUNT(rental.rental_id) AS 'number of rentals',
+    SUM(payment.amount) AS 'total paid',
+    DATE(rental.rental_date) AS 'rental date'
 FROM
     rental
-        INNER JOIN
-    payment ON rental.customer_id = payment.customer_id
-        RIGHT JOIN
-    customer ON payment.customer_id = customer.customer_id
-GROUP BY  customer_id , rental.rental_id;
+        LEFT JOIN
+    payment ON rental.rental_id = payment.rental_id
+        LEFT JOIN
+    customer ON rental.customer_id = customer.customer_id
+GROUP BY rental.rental_date , rental.customer_id
+HAVING COUNT(payment.rental_id) >= 2; 
