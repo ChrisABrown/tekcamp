f8bd3eefcb44fa60b1fd44fe866f07a4089961b7
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-09-21 14:04:13 -0500

    (Chore): Adds TODOs to applicable files and adjusts README.md to better document updates

--- Capstone1/suprime-app/README.md
+++ Capstone1/suprime-app/README.md
@@ -1,1 +1,10 @@
 # Supreme Mock Site
+
+In order to run site at root:
+
+```
+ npm i
+ npm run start
+```
+
+Tests to be added...
--- Capstone1/suprime-app/src/App.jsx
+++ Capstone1/suprime-app/src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useContext, createContext } from "react";
+//TODO: Clean up files, current build does not have login capabilities, nor does it have checkout page for the cart,
+
+import React, { useState, useContext, createContext } from 'react'
 import {
   BrowserRouter as Router,
   Routes,
@@ -8,24 +10,24 @@ import {
   useLocation,
   Navigate,
   Outlet,
-} from "react-router-dom";
-import FrontPage from "./Components/Pages/FrontPage";
-import Contact from "./Components/Pages/Forms/Contact";
-import Shop from "./Components/Pages/Shop/Shop";
-import AdminView from "./Components/Pages/Forms/AdminView";
-import Cart from "./Components/Pages/Shop/Checkout/Cart";
-import inventory from "./Data/inventory";
+} from 'react-router-dom'
+import FrontPage from './Components/Pages/FrontPage'
+import Contact from './Components/Pages/Forms/Contact'
+import Shop from './Components/Pages/Shop/Shop'
+import AdminView from './Components/Pages/Forms/AdminView'
+import Cart from './Components/Pages/Shop/Checkout/Cart'
+import inventory from './Data/inventory'
 
 export default function App() {
-  const [items, setItems] = useState(inventory.items);
+  const [items, setItems] = useState(inventory.items)
 
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<FrontPage />} />
+        <Route path='/' exact element={<FrontPage />} />
 
         <Route
-          path="/admin"
+          path='/admin'
           element={
             // <RequireAuth>
             <AdminView key={inventory} stock={items} />
@@ -33,16 +35,16 @@ export default function App() {
         />
 
         <Route
-          path="/shop"
+          path='/shop'
           exact
           element={<Shop key={items.id} items={items} setItems={setItems} />}
         ></Route>
-        <Route path="/shop/cart" exact element={<Cart />} />
+        <Route path='/shop/cart' exact element={<Cart />} />
 
-        <Route path="/contact" exact element={<Contact />} />
+        <Route path='/contact' exact element={<Contact />} />
 
         {/* <ProtectedRoute path="/admin" component={AdminView} isAuth={isAuth} /> */}
       </Routes>
     </Router>
-  );
+  )
 }
