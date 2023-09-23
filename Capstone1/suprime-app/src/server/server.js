3301a6673debfde4fe90cc6333309088158d247a
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-09-23 14:20:13 -0500

    (Backend): Adds mongodb connection code to server.js and excludes .pem files from github

--- Capstone1/suprime-app/src/server/server.js
+++ Capstone1/suprime-app/src/server/server.js
@@ -1,12 +1,33 @@
 const { default: mongoose } = require('mongoose')
 
 const db = require('../config/keys').mongodbURI
+const express = require('express')
+const cors = require('cors')
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.log('DBerror', err))
+const app = express()
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3030
+app.use(cors())
+app.use(express.json())
 
-app.listen()
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tlsCertificateKeyFile: 'CA.pem',
+})
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
+
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy')
+  app.use((req, res, next) => {
+    if (req.secure) next()
+    else res.redirect(`https://'${req.headers.host}${req.url}`)
+  })
+}
