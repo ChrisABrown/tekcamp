0d2b50301257bd3dd86f8925e521c3d64cadff11
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-10-01 00:09:37 -0500

    (Backend): Verifies that Authentication and any other Backend work cannot be finished until frontend is up and ready

--- Capstone1/suprime-app/backend/server.js
+++ Capstone1/suprime-app/backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import router from './API/routes/items.route.js'
 import router2 from './API/routes/users.route.js'
+import auth from 'express-openid-connect'
 
 const app = express()
 const itemsRouter = router
@@ -13,6 +14,23 @@ app.use('/api/v1/items', itemsRouter)
 app.use('/api/v1/users', usersRouter)
 app.use('*', (req, res) => {
   res.status(404).json({ error: 'not found' })
+})
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: 'http://localhost:4040',
+  clientID: 'VhmXU9D5jPV3tViazHR6da0nWXpL96Qu',
+  issuerBaseURL: 'https://dev-xxofmt70.us.auth0.com',
+}
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config))
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 })
 
 export default app
